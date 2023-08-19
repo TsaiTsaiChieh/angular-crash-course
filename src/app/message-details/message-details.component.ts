4229ec48581511824dac4afc9aaa058d1454b2c8
Tsai-Chieh <d0177381@gmail.com>
2023-08-19 16:25:12 +0800

    :sparkles: get input from parent component

--- src/app/message-details/message-details.component.ts
+++ src/app/message-details/message-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message-details',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-details.component.scss']
 })
 export class MessageDetailsComponent {
-
+  @Input()
+  message: any = {}
 }
