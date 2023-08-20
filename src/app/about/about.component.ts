1432043962370db091daba3fe8a6a7e3c432c512
Tsai-Chieh <d0177381@gmail.com>
2023-08-20 18:10:28 +0800

    :sparkles: get param from URL

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  param: any
 
+  constructor(private activatedRouter: ActivatedRoute) {
+    
+  } 
+
+  
+  ngOnInit(): void {
+    console.log(this.activatedRouter)
+    this.param = this.activatedRouter.snapshot.params['username']
+  }
 }
