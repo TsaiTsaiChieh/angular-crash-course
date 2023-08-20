51d105eb787b2a032ce63aade0284e3291bf753e
Tsai-Chieh <d0177381@gmail.com>
2023-08-20 20:59:31 +0800

    :sparkles: show product details in product page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {ProductRepresentation} from '../services/api/models/product-representation';
+import {ProductService} from '../services/api/products/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: ProductRepresentation[] = []
+  constructor(private service: ProductService) {
 
+  }
+  
+  ngOnInit (): void {
+    this.service.getAllProductWithLimit().subscribe({next: 
+    (result)=> {
+      this.products = result
+    }})
+  }
 }
