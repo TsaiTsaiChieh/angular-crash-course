6e0c8f4be8d3fbc6060f92d50fd426e6e2730807
Tsai-Chieh <d0177381@gmail.com>
2023-08-20 21:57:40 +0800

    :sparkles: connect saveProduct logic

--- src/app/new-product/new-product.component.ts
+++ src/app/new-product/new-product.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ProductRepresentation } from '../services/api/models/product-representation';
+import { ProductService } from '../services/api/products/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.scss']
+  styleUrls: ['./new-product.component.scss'],
 })
 export class NewProductComponent {
+  constructor(private service: ProductService, private router: Router) {}
+  product: ProductRepresentation = {};
 
+  saveProduct() {
+    // console.log(this.product)
+    this.service.createProduct(this.product).subscribe({
+      next: () => {
+        this.router.navigate(['products']);
+      },
+    });
+  }
 }
